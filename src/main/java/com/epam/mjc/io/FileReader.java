7a758ea6bceda77cce60af5dfc0154aaa4b8974d
IsNotAvailableName <148902549+IsNotAvailableName@users.noreply.github.com>
2023-12-25 19:07:34 +0300

    Done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,34 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        String name = null;
+        Integer age = null;
+        String email = null;
+        Long phone = null;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))) {
+            name = bufferedReader
+                    .readLine()
+                    .replaceFirst("Name: ", "");
+            age = Integer.parseInt(bufferedReader
+                    .readLine()
+                    .replaceFirst("Age: ", ""));
+            email = bufferedReader.readLine().replaceFirst("Email: ", "");
+            phone = Long.parseLong(bufferedReader
+                    .readLine()
+                    .replaceFirst("Phone: ", ""));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return new Profile(name, age, email, phone);
     }
 }
